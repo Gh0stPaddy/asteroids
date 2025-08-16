fa48fdc470268780946c2a909ad04982cae229f3
Gh0stPaddy <paddy.foakes@gmail.com>
2025-08-16 12:08:43 +0100

    [X] Button now working with loop in correct placement

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    # Screen closed check
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    
 
     # Game Loop
     while True:
         pygame.Surface.fill(screen, (0,0,0))
+        # Enables the ability to press the [X] button at the top right
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
