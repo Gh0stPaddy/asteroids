6950e93ce3df0316463e3498933bdf4632527c35
Gh0stPaddy <paddy.foakes@gmail.com>
2025-08-16 12:57:08 +0100

    player now renders after fixing main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     # Clock and delta time for FPS
     clock = pygame.time.Clock()
     dt = 0
-    
+    # Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+ 
 
 
     # Game Loop
@@ -20,10 +21,12 @@ def main():
                 return
         # Screen Fill
         screen.fill("black")
+        # drawing player in game
+        player.draw(screen)
         pygame.display.flip()
         # Framerate limiter to 60 FPS
         dt = clock.tick(60) / 1000
-            
+
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,11 +1,15 @@
+import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 
-class Player(CircleShape):
-    def __init__(self, x: int, y: int):
-        super().__init__(x, y, PLAYER_RADIUS)
 
-    rotation = 0
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     # in the player class
     def triangle(self):
