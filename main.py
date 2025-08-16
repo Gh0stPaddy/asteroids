74fece8de7441ab95643e5ad6cc3f5ac34e0633a
Gh0stPaddy <paddy.foakes@gmail.com>
2025-08-16 12:11:20 +0100

    Small change with while loop

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
 
     # Game Loop
     while True:
-        pygame.Surface.fill(screen, (0,0,0))
         # Enables the ability to press the [X] button at the top right
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Screen Fill
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
