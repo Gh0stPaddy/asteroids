6e694f9c2a3e6bad9d995d34655ba7bae22ef580
Gh0stPaddy <paddy.foakes@gmail.com>
2025-08-16 12:21:54 +0100

    added clock and FPS limiter

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Clock and delta time for FPS
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
 
     # Game Loop
     while True:
@@ -15,7 +21,9 @@ def main():
         # Screen Fill
         screen.fill("black")
         pygame.display.flip()
-
+        # Framerate limiter to 60 FPS
+        dt = clock.tick(60) / 1000
+            
 
 if __name__ == "__main__":
     main()
